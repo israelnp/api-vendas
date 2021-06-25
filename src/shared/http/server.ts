0fee207c19c426078b8600aaeb46e2fced161e56
israelnp <israelpereira1984@gmail.com>
2021-06-24 21:09:22 -0300

    add typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routers';
 import AppError from '@shared/errors/AppErros';
+import '@shared/typeorm';
 
 const app = express();
 
